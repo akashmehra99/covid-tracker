54b530e46452ef241a8e3d9aa192ba56bee9e9da
Akash MEHRA <akash.mehra99@gmail.com>
2020-04-21 19:10:59 +0530

    Covid tracker

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Container from 'components/Container';
+
+const Footer = () => {
+  return (
+    <footer>
+      <Container>
+        <p>
+          <a href="https://www.linkedin.com/in/akash-mehra-980a6181/">Made by @akashmehra</a>
+        </p>
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
